f174e078aee61390aa1627170cb5bea50cfaf47a
Enegg <42005170+Enegg@users.noreply.github.com>
2023-08-23 14:52:25 +0200

    Include toml en/decoders

--- src/supermechs/platform.py
+++ src/supermechs/platform.py
@@ -1,13 +1,20 @@
+import sys
 import typing as t
 
-__all__ = ("json_decoder", "compact_json_encoder", "indented_json_encoder")
+__all__ = (
+    "json_decoder",
+    "compact_json_encoder",
+    "indented_json_encoder",
+    "toml_decoder",
+    "toml_encoder",
+)
 
-JSONDecoder = t.Callable[[str | bytes], t.Any]
-JSONEncoder = t.Callable[[t.Any], bytes]
+DecoderType = t.Callable[[str | bytes], t.Any]
+EncoderType = t.Callable[[t.Any], bytes]
 
-json_decoder: JSONDecoder
-compact_json_encoder: JSONEncoder
-indented_json_encoder: JSONEncoder
+json_decoder: DecoderType
+compact_json_encoder: EncoderType
+indented_json_encoder: EncoderType
 
 try:
     import orjson  # pyright: ignore[reportMissingImports]
@@ -29,3 +36,19 @@ else:
 
     def indented_json_encoder(obj: t.Any, /) -> bytes:
         return orjson.dumps(obj, option=orjson.OPT_INDENT_2)
+
+
+toml_encoder: EncoderType
+toml_decoder: DecoderType
+
+if sys.version_info >= (3, 11):
+    import tomlib
+
+else:
+    import rtoml
+
+    def toml_decoder(s: str | bytes, /) -> t.Any:
+        return rtoml.loads(s.decode() if isinstance(s, bytes) else s)
+
+    def toml_encoder(o: t.Any, /) -> bytes:
+        return rtoml.dumps(o).encode()
