e1f67a75c7c033f504b75e6168a67d948d77f0df
Enegg <42005170+Enegg@users.noreply.github.com>
2023-09-08 19:00:59 +0200

    Provide `__init__` for submodules

---
+++ src/supermechs/ext/deserializers/__init__.py
@@ -0,0 +1,19 @@
+from .models import (
+    extract_key,
+    to_item_data,
+    to_item_pack,
+    to_stats_mapping,
+    to_tags,
+    to_transform_range,
+    to_transform_stages,
+)
+from .graphic import to_attachments, to_pack_renderer
+
+__all__ = (
+    "to_tags", "to_transform_range",
+    "to_stats_mapping", "to_transform_stages",
+    "to_item_data",
+    "to_item_pack", "extract_key",
+    "to_attachments", "to_pack_renderer"
+)
+
---
+++ src/supermechs/ext/deserializers/typedefs/__init__.py
@@ -0,0 +1,4 @@
+from .game_types import LiteralElement, LiteralType
+from .packs import AnyItemPack
+
+__all__ = ("AnyItemPack", "LiteralElement", "LiteralType")
