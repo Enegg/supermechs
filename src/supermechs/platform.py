3685ae71bc25bf2c25aff0b9713febce27d14f91
Enegg <42005170+Enegg@users.noreply.github.com>
2023-09-08 22:06:56 +0200

    Import rtoml no matter the version; dedent arrays

--- src/supermechs/platform.py
+++ src/supermechs/platform.py
@@ -1,5 +1,7 @@
-import sys
+import re
 import typing as t
+
+import rtoml
 
 __all__ = (
     "json_decoder",
@@ -34,21 +36,21 @@ else:
     json_decoder = orjson.loads
     compact_json_encoder = orjson.dumps
 
+    _indented_array = re.compile(rb",\n\s+(\d+)")
+
+
     def indented_json_encoder(obj: t.Any, /) -> bytes:
-        return orjson.dumps(obj, option=orjson.OPT_INDENT_2)
+        data = orjson.dumps(obj, option=orjson.OPT_INDENT_2)
+        data = _indented_array.sub(lambda match: b", " + match[1], data)
+        return data
 
 
 toml_encoder: EncoderType
 toml_decoder: DecoderType
 
-if sys.version_info >= (3, 11):
-    import tomlib
 
-else:
-    import rtoml
+def toml_decoder(s: str | bytes, /) -> t.Any:
+    return rtoml.loads(s.decode() if isinstance(s, bytes) else s)
 
-    def toml_decoder(s: str | bytes, /) -> t.Any:
-        return rtoml.loads(s.decode() if isinstance(s, bytes) else s)
-
-    def toml_encoder(o: t.Any, /) -> bytes:
-        return rtoml.dumps(o).encode()
+def toml_encoder(o: t.Any, /) -> bytes:
+    return rtoml.dumps(o).encode()
