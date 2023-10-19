85ea5cd371e8a8d2d4cb15f41ca07b76d17c0595
Enegg <42005170+Enegg@users.noreply.github.com>
2023-10-19 11:08:10 +0200

    Remove inexistent import

--- src/supermechs/ext/deserializers/__init__.py
+++ src/supermechs/ext/deserializers/__init__.py
@@ -4,13 +4,12 @@ from .models import (
     to_item_pack,
     to_stats_mapping,
     to_tags,
-    to_transform_range,
     to_transform_stages,
 )
 from .graphic import to_attachments, to_pack_renderer
 
 __all__ = (
-    "to_tags", "to_transform_range",
+    "to_tags",
     "to_stats_mapping", "to_transform_stages",
     "to_item_data",
     "to_item_pack", "extract_key",
