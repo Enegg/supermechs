8cae026292d71c9e855343be182bd740b3fd4c2d
Enegg <42005170+Enegg@users.noreply.github.com>
2023-10-24 15:30:28 +0200

    Move import inside init

--- src/supermechs/__init__.py
+++ src/supermechs/__init__.py
@@ -1,10 +1,10 @@
 import logging
 
-from ._internal import load_power_data, load_stat_data
-
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
 async def init() -> None:
+    from ._internal import load_power_data, load_stat_data
+
     await load_power_data()
     await load_stat_data()
