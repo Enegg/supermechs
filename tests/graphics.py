326ebb3191cf015719bdd7ce3d9b77ffe5bab9f7
Enegg <42005170+Enegg@users.noreply.github.com>
2024-02-21 03:55:28 +0100

    Move tests & exclude data files

--- tests/graphics.py
+++ tests/graphics.py
@@ -1,8 +1,8 @@
+from supermechs.abc.item_pack import PackKey
 from supermechs.api import ItemPack, Mech, PackData
-from supermechs.rendering import get_mech_graphics
-from supermechs.typeshed import PackKey
+from supermechs.graphics.api import get_mech_gfx
 
 mech = Mech(name="Test build")
 pack = ItemPack(data=PackData(PackKey("@Default")), items={}, sprites={})
 
-graphics = get_mech_graphics(mech, pack.get_sprite)
+graphics = get_mech_gfx(mech, pack.get_sprite)
